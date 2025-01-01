a2b6b6026e012dcefede43bd078e6ac1d8a46eeb
Camden Webster <59626059+camdenwebster@users.noreply.github.com>
2025-01-01 06:54:17 -0600

    Initial commit

---
+++ Cart Copilot/Cart_CopilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cart_CopilotApp.swift
+//  Cart Copilot
+//
+//  Created by Camden Webster on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Cart_CopilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
